864233ee3f51ca3bb0e06711c49cc54d8509f485
Jujumba <far77578@gmail.com>
2025-04-18 00:20:22 +0200

    change nvim highlights

--- nvim/lua/highlight.lua
+++ nvim/lua/highlight.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_set_hl(0, "Number", { ctermfg = "blue" })
-vim.api.nvim_set_hl(0, "Keyword", { ctermfg = "red", })
+vim.api.nvim_set_hl(0, "Keyword", { ctermfg = "lightyellow", })
 vim.api.nvim_set_hl(0, "Statement", { ctermfg = "red", })
 vim.api.nvim_set_hl(0, "Constant", { ctermfg = "darkblue" })
 vim.api.nvim_set_hl(0, "PreProc", { })
@@ -7,18 +7,22 @@ vim.api.nvim_set_hl(0, "@string.escape", { ctermfg = "darkblue" })
 vim.api.nvim_set_hl(0, "@character", { ctermfg = "green" })
 vim.api.nvim_set_hl(0, "Comment", { ctermfg = "darkgreen" })
 vim.api.nvim_set_hl(0, "Delimiter", { ctermfg = "gray" })
-vim.api.nvim_set_hl(0, "Type", { ctermfg = "cyan", bold = true })
+vim.api.nvim_set_hl(0, "Type", { ctermfg = "blue" })
 vim.api.nvim_set_hl(0, "Operator", { ctermfg = "red" })
+vim.api.nvim_set_hl(0, "Function", { link = "Normal" })
+vim.api.nvim_set_hl(0, "@variable", { link = "Normal" })
 
 
 -- Rust specific
 vim.api.nvim_set_hl(0, "rustCharacter", { ctermfg = "green" })
 vim.api.nvim_set_hl(0, "rustSigil", { ctermfg = "red" })
-vim.api.nvim_set_hl(0, "@lsp.type.const.rust", { ctermfg = "darkyellow" })
+vim.api.nvim_set_hl(0, "@lsp.type.const.rust", { link = "Normal" })
 vim.api.nvim_set_hl(0, "rustFuncCall", { link = "Normal" })
 vim.api.nvim_set_hl(0, "@lsp.type.method.rust", { link = "Normal" })
-vim.api.nvim_set_hl(0, "rustStorage", { ctermfg = "red", bold = true })
+vim.api.nvim_set_hl(0, "rustStorage", { link = "Keyword" })
 vim.api.nvim_set_hl(0, "rustModPath", { link = "Normal" })
+vim.api.nvim_set_hl(0, "rustEscape", { ctermfg = "lightblue" })
+vim.api.nvim_set_hl(0, "@lsp.type.macro.rust", { link = "Normal", italic = true })
 
 
 -- Other language specific
