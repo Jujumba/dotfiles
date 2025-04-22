7e79c5a2d70e25600f8e77a2f316b109702b62e4
Farid Huliiev <farid@jujumba.cc>
2025-04-22 23:46:20 +0200

    nvim: don't autoclose single quotes for rust

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -1,1 +1,6 @@
-require('autoclose').setup()
+require('autoclose').setup({
+    -- Disable single quote completion for rust (lifetimes aahhh)
+    keys = {
+        ["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "rust" } },
+    }
+})
