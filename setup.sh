1d6202b6bcb643167043e98b102162979d6fd65b
Farid Huliiev <farid@jujumba.cc>
2025-04-22 14:10:29 +0200

    setup.sh: update stow symlinks

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/sh
-stow -t ~/.config .
+# all dirs here (except `.` and `.git`) -> ~/.config
+stow -t ~/.config $(find -maxdepth 1 -not -path '.' -not -name .git -type d -printf '%P\n')
+# git config -> ~ 
+stow -t ~ --no-folding git
