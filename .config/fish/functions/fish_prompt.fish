2e1604d6bfea756081d1909ed5019c0aab454ea0
Farid <farid@jujumba.cc>
2025-06-05 16:58:39 +0200

    fish: update prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,11 +1,5 @@
 function fish_prompt
     set -l last_status $status
 
-    if test $status -ne 0
-        set dollar (set_color brred) ' > ' (set_color normal)
-    else
-        set dollar (set_color cyan) ' > ' (set_color normal)
-    end
-
-    string join '' (set_color green) (prompt_pwd) (set_color normal) (fish_git_prompt) $dollar
+    string join '' (set_color green) (prompt_pwd) (set_color normal) (fish_git_prompt) (set_color cyan) ' > ' (set_color normal)
 end
---
+++ .config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,19 @@
+function fish_right_prompt
+    set -l last_status $status
+
+    if test $status -ne 0
+        set prompt_status (set_color red) $last_status (set_color normal)
+    else
+        set prompt_status (set_color normal) '' 
+    end
+
+    set slash  (set_color green) "/" (set_color normal)
+    set colon  (set_color green) ":" (set_color normal)
+    set day    (set_color brmagenta) (date +%d) (set_color normal)
+    set month  (set_color brmagenta) (date +%m) (set_color normal)
+    set year   (set_color brmagenta) (date +%Y) (set_color normal)
+    set hour   (set_color brmagenta) (date +%H)  (set_color normal)
+    set minute (set_color brmagenta) (date +%M)  (set_color normal)
+
+    string join '' $prompt_status ' ' $day $slash $month $slash $year ' ' $hour $colon $minute
+end
