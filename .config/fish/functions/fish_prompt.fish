5ec6327a7bcc50bc386d9f92901eaa0428094aa7
Farid <farid@jujumba.cc>
2025-06-19 11:28:57 +0200

    fish: update prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,10 @@
 function fish_prompt
     set -l last_status $status
+    if test $last_status -ne 0
+        set dollar (set_color red) ' $ ' (set_color normal)
+    else
+        set dollar (set_color green) ' $ ' (set_color normal)
+    end
 
-    string join '' (set_color green) (prompt_pwd) (set_color normal) (fish_git_prompt) (set_color cyan) ' > ' (set_color normal)
+    string join '' (set_color green) (prompt_pwd) (set_color normal) (fish_git_prompt) (set_color cyan) $dollar (set_color normal)
 end
