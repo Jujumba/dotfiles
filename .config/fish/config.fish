71c20b5002bfaa53bf573d1e968b4b346d53ecb0
Farid <farid@jujumba.cc>
2025-07-14 23:53:17 +0200

    fish: more binds & cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 zoxide init fish | source
 
+# Copy Working Directory
 function cwd
     pwd | wl-copy
 end
@@ -12,9 +13,10 @@ alias ipy=ipython
 alias py=python
 alias cl=clear
 alias objdump="objdump -M intel"
-# alias ls="eza -A --time-style=long-iso"
+alias clip="wl-copy"
 
 # Git aliases
+alias g="git"
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit"
@@ -22,11 +24,31 @@ alias gd="git diff"
 alias gp="git push"
 alias gl="git pull"
 
-set PATH $PATH  /home/jujumba/.cargo/bin /home/jujumba/.local/bin
+set PATH $PATH  /home/$USER/.cargo/bin
 
 set fish_greeting ""
-set fish_color_command --bold cyan
+set fish_color_command --bold brblue
 
 bind \e\t forward-char
-bind \ek up-or-search   # Alt + k
-bind \ej down-or-search # Alt + j
+bind \el forward-char           # Alt + l -> right
+bind \ek up-or-search           # Alt + k -> up
+bind \ej down-or-search         # Alt + j -> down
+bind \ec fish_clipboard_paste   # Alt + c -> paste
+bind \ee "nvim_fzf_edit"        # Alt + e -> find file and edit
+bind \ef "append_fzf_search"    # Alt + f -> find file and append to prompt
+
+function nvim_fzf_edit
+    set fzf_result (fzf)
+    if test $status -ne 0
+        return
+    end
+    nvim $fzf_result
+end
+
+function append_fzf_search
+    set fzf_result (fzf)
+    if test $status -ne 0
+        return
+    end
+    commandline --insert $fzf_result
+end
