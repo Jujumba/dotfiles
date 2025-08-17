e6f8bf0a88f787f81b304143d1a399ce4c6b605a
Farid <farid@jujumba.cc>
2025-08-18 00:02:59 +0200

    fish: change aliases + fix fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,8 +7,6 @@ end
 
 # Aliases
 alias cd=z
-alias p=poweroff
-alias m="math '$argv'"
 alias ipy=ipython
 alias py=python
 alias cl=clear
@@ -28,6 +26,7 @@ set PATH $PATH  /home/$USER/.cargo/bin
 
 set fish_greeting ""
 set fish_color_command --bold blue
+set fish_color_param brblue
 
 bind \e\t forward-char
 bind \ek up-or-search           # Alt + k -> up
@@ -37,20 +36,22 @@ bind \ee "nvim_fzf_edit"        # Alt + e -> find file and edit
 bind \ef "append_fzf_search"    # Alt + f -> find file and append to prompt
 
 function nvim_fzf_edit
-    set fzf_result (fzf --border --color=16 --height ~100% --layout reverse)
-    commandline --function repaint  # clear fzf output
-    if test $status -ne 0
+    set fzf_result (fzf --border --color=16 --height 30% --layout reverse)
+    set fzf_status $status
+    commandline --function repaint
+    if test $fzf_status -ne 0
         return
     end
     commandline --replace "nvim $fzf_result"
     commandline --function execute
-    # commandline --function repaint
+    commandline --function repaint
 end
 
 function append_fzf_search
-    set fzf_result (fzf --border --color=16 --height ~100% --layout reverse)
+    set fzf_result (fzf --border --color=16 --height 30% --layout reverse)
+    set fzf_status $status
     commandline --function repaint  # clear fzf output
-    if test $status -ne 0
+    if test $fzf_status -ne 0
         return
     end
     commandline --insert $fzf_result
