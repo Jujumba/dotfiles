acbc1fbf3935c4080b929db406eab6f442874a33
Jujumba <farid@jujumba.cc>
2026-02-20 00:34:19 +0100

    disable autoclosing for '

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -1,6 +1,5 @@
 require('autoclose').setup({
-    -- Disable single quote completion for rust (lifetimes aahhh)
     keys = {
-        ["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "rust" } },
+        ["'"] = { escape = true, close = false, pair = "''", disabled_filetypes = {} },
     }
 })
